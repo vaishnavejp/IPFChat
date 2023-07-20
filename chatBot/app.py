59b1d7f82c0613049b200b16246a32a5b7ed626c
vaishnave3 <vaishnavejp3@gmail.com>
2023-07-20 17:53:35 +0530

    pre-final changes. must add comments

--- chatBot/app.py
+++ chatBot/app.py
@@ -8,8 +8,13 @@ from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 import streamlit as st
 from streamlit_chat import message
+from langchain import PromptTemplate
+from PIL import Image
 
 st.set_page_config(page_title="ChatBot")
+image = Image.open('logo.png')
+
+st.image(image, width=100)
 
 load_dotenv()
 
@@ -22,13 +27,27 @@ db = Chroma(persist_directory=persist_dir, embedding_function=embeddings)
 
 @st.cache_resource
 def search_db():
+
+    prompt_template = """If the context is not relevant, 
+    please say Im sorry, but I currently dont have enough context to answer your question. 
+    
+    {context}
+    
+    Question: {question}
+    """
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain_type_kwargs = {"prompt": PROMPT}
     retriever = db.as_retriever()
     retriever.search_kwargs["distance_metric"] = "cos"
     retriever.search_kwargs["fetch_k"] = 100
     retriever.search_kwargs["maximal_marginal_relevance"] = True
     retriever.search_kwargs["k"] = 10
     model = ChatOpenAI(model="gpt-3.5-turbo")
-    qa = RetrievalQA.from_llm(model, retriever=retriever, return_source_documents=True)
+    qa = RetrievalQA.from_chain_type(model, retriever=retriever, return_source_documents=True, chain_type_kwargs=chain_type_kwargs)
     return qa
 
 
@@ -43,21 +62,12 @@ def display_conversation(history):
             is_user=True,
             key=str(i) + "_user",
         )
-        # message(history["generated"][i], key=str(i), avatar_style="initials", seed="AI")
-        with st.chat_message("assisstant", avatar="./logo.png"):
-            st.markdown(
-                "<font size='+2'>" + history["generated"][i],
-                unsafe_allow_html=True,
-            )
+        message(history["generated"][i], key=str(i), avatar_style="initials", seed="AI")
 
 
 def main():
-    query = "how to use an icon provided aws environment?"
-    output = qa({"query": query})
-    print(output["result"])
-
     # Initialize Streamlit app with a title
-    st.title("IPF Documentation Chatbot")
+    st.title("IPF Developer Chatbot")
 
     with st.expander("About the Chatbot"):
         st.markdown(
@@ -66,7 +76,7 @@ def main():
             *The better the prompt, the better the answer.*\n
             Examples : 
             - what is a connector? 
-            - how to create a project from scratch?\n
+            - how to create a project?\n
             You can refer to this [https://docs.ipfdev.co.uk/home/RELEASE-IPF-2023.1.0/home.html] for further information.
             """
         )
@@ -74,9 +84,10 @@ def main():
     # Get user input from text input
     user_input = st.text_input("", placeholder="Enter your prompt here", key="input")
 
+
     # Initialize session state for generated responses and past messages
     if "generated" not in st.session_state:
-        st.session_state["generated"] = ["I am ready to help you"]
+        st.session_state["generated"] = ["Ask away any questions you have about the IPF documentation."]
     if "past" not in st.session_state:
         st.session_state["past"] = ["Hey there!"]
 
