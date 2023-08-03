2ec574ba164111af537349822b13efab0af27400
J P VAISHNAVE <70100937+vaishnavejp@users.noreply.github.com>
2023-08-03 07:39:45 +0100

    Update crawl.py

--- data_collection/webScrape/crawl.py
+++ data_collection/webScrape/crawl.py
@@ -5,6 +5,7 @@ import requests
 # EXAMPLE - fd = open("./textFiles/crawlURL.txt", "r", encoding="utf-8")
 
 fd = open("<YOUR FILE PATH>", "r", encoding="utf-8")
+
 linksRead = fd.read().splitlines()
 
 links = []
@@ -15,16 +16,22 @@ content = ""
 
 # Manually add base path link
 # EXAMPLE - c = "https://docs.ipfdev.co.uk/learn/RELEASE-IPF-2023.1.0/tutorials/"
+
 c = "<YOUR BASE PATH>"
 
 for i in links:
     file = requests.get(i)
 
     soup = BeautifulSoup(file.content, "lxml")
-    tags = soup.find_all("li", class_="nav-item")
+
+    # Inspect and find out what tags to scrape to collect urls
+    # EXAMPLE - tags = soup.find_all("li", class_="nav-item")
+    tags = soup.find_all("<HTML TAG>", class_="<CLASS NAME OF TAG>")
 
     for j in tags:
-        x = j.find("a", class_="nav-link", href=True)
+        # Add the hyperlinks and its corresponding class name
+        # EXAMPLE - x = j.find("a", class_="nav-link", href=True)
+        x = j.find("a", class_="<CLASS NAME OF ANCHOR TAG>", href=True)
         if x is not None:
             content += x["href"] + "\n"
 
