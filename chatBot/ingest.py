c6265dc42baf7afe625ced625371df0c21d92c4f
VaishnaviPanchatcharam7 <141195915+VaishnaviPanchatcharam7@users.noreply.github.com>
2023-08-03 09:53:04 +0100

    Update ingest.py

--- chatBot/ingest.py
+++ chatBot/ingest.py
@@ -10,47 +10,42 @@ import streamlit as st
 from streamlit_chat import message
 
 load_dotenv()
-
+#set open api key from .env
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-#os.environ['ACTIVELOOP_TOKEN'] = os.getenv('ACTIVELOOP_TOKEN')
-#os.environ['DEEPLAKE_ACCOUNT_NAME']= os.getenv('DEEPLAKE_ACCOUNT_NAME')
-
+# directory to store embedding
 persist_dir="db"
 
 @st.cache_data
 def doc_preprocessing():
+    #loading data from pdfs
     loader = DirectoryLoader(
         'data/',
         glob='**/*.pdf',     # only the PDFs
         show_progress=True
     )
     docs = loader.load()
+    # initialising chunk size to 1000 and chunk overlap to 0
     text_splitter = CharacterTextSplitter(
         chunk_size=1000, 
         chunk_overlap=0
     )
+    # splitting the documents into chunks and returning them
     docs_split = text_splitter.split_documents(docs)
     return docs_split
 
 @st.cache_resource
 def embeddings_store():
+    #embedding using open ai api
     embeddings = OpenAIEmbeddings()
-    print(embeddings)
     texts = doc_preprocessing()
     persist_dir='db'
+    #generating the embeddings and storing it in 'db' directory
     db = Chroma.from_documents(documents=texts,embedding=embeddings,persist_directory=persist_dir)
-    print(db)
     db.persist()
     db=None
-    #db = DeepLake(
-    #dataset_path=f"hub://aianytime07/text_embedding",
-    #read_only=True,
-    #embedding_function=embeddings,
-    #)
 
 def main():
     embeddings_store()
     
 if __name__ == "__main__":
     main()
-
